6f554753ea2377b747464a51351c2f0bbc7630f6
Xavier <xavier.law@ontariotechu.net>
2020-11-22 14:25:08 -0500

    Added Object Pooling
    -Object pooling implemented
    -Currently we aren't using it as we haven't gotten to designing our powerups, etc.

---
+++ Assets/Scripts/Tutorial/ItemController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemController : MonoBehaviour
+{
+    public GameObject player;
+
+    public ItemPoolManager itemPoolManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        itemPoolManager = FindObjectOfType<ItemPoolManager>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.collider.name == "Player")
+        {
+            itemPoolManager.ResetItem(this.gameObject);
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Tutorial/ItemPoolManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//modified from individual in class assignment 1
+[System.Serializable]
+public class ItemPoolManager : MonoBehaviour
+{
+    public GameObject item;
+
+    public int maxItems;
+
+    Queue<GameObject> itemQueue = new Queue<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BuildItemPool();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void BuildItemPool()
+    {
+        for (int i = 0; i < maxItems; i++)
+        {
+            GameObject tempItem = Instantiate(item, new Vector3(Random.Range(0f,20f), Random.Range(0f,20f), 0.0f), Quaternion.identity);
+            tempItem.SetActive(false);
+            itemQueue.Enqueue(tempItem);
+        }
+    }
+
+    public GameObject GetItem()
+    {
+        GameObject tempItem;
+        //if Queue is not empty
+        if (!QueueIsEmpty())
+        {
+            tempItem = itemQueue.Dequeue();
+            tempItem.SetActive(true);
+        }
+        else
+        {
+            tempItem = MonoBehaviour.Instantiate(item, new Vector3(Random.Range(0f, 0.5f), Random.Range(0f, 0.5f), 0.0f), Quaternion.identity);
+            maxItems += 1;
+        }
+        return tempItem;
+    }
+
+    public void ResetItem(GameObject item)
+    {
+        //Move position back to pool
+        item.transform.position = new Vector3(Random.Range(0f, 20f), Random.Range(0f, 20f), 0.0f);
+        //Deactivate it since its back in pool
+        item.SetActive(false);
+        //Actually queue it back into the pool
+        itemQueue.Enqueue(item);
+    }
+
+    //function to return item pool size
+    public int ItemPoolSize()
+    {
+        return itemQueue.Count;
+    }
+    public bool QueueIsEmpty()
+    {
+        return ItemPoolSize() == 0;
+    }
+
+}
