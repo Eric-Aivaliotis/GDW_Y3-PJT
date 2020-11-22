9a587f49a8fb6c704af8673f10b2c84938e075ab
Xavier <xavier.law@ontariotechu.net>
2020-11-22 13:06:52 -0500

    Added Quest
    Visual quests that players can complete in the tutorial

---
+++ Assets/Scripts/Tutorial/QuestManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuestManager : MonoBehaviour
+{
+    public GameObject plane;
+    public GameObject jumpPlatform;
+    public GameObject flagPlane;
+
+    bool moveQuest;
+    bool jumpQuest;
+    bool pointQuest;
+
+    public Text Quests;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       moveQuest = false;
+       jumpQuest = false;
+       pointQuest = false;
+
+        Quests.text = "Learn to Move [ ]" +
+            "\nMaster the Jump [ ]" +
+            "\nCapture the Flag [ ] ";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(moveQuest == true)
+        {
+            Quests.text = "Learn to Move [x]" +
+                "\nMaster the Jump [ ]" +
+                "\nCapture the Flag [ ] ";
+            if(jumpQuest == true)
+            {
+                Quests.text = "Learn to Move [x]" +
+               "\nMaster the Jump [x]" +
+               "\nCapture the Flag [ ] ";
+            }
+            if(pointQuest == true)
+            {
+                Quests.text = "Learn to Move [x]" +
+               "\nMaster the Jump [x]" +
+               "\nCapture the Flag [x] ";
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+       
+        if(other.collider.name == "Plane")
+        {
+            moveQuest = true;
+        }
+        if (other.collider.name == "Jump Platform")
+        {
+            jumpQuest = true;
+        }
+        if (other.collider.name == "FlagPlane")
+        {
+            pointQuest = true;
+        }
+    }
+}
